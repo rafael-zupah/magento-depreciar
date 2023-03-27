14a1b94ea692c264fc998dd5f12132ef3489bc1b
Rafael <rafael@maisvenda.com.br>
2023-03-27 20:02:39 -0300

    melhorias de documentação e ajuste de nomes

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # magento-243
 Magento Versão 2.4.3
+
+**Criando base de dados**
+  ```sh
+    rsync -a root@loja.meece.com.br:/var/www/dump.sql ./docker/mysql/dumps
+  ```
+
+  ```sh
+    make mysql
+  ```
+
+  ```sh
+    create database magento;
+    use magento;
+    source /tmp/dumps/dump.sql;
+    exit;
+  ```
+
+**Instalando Magento no Servidor PHP**
+  ```sh
+    make bash
+  ```
+
+  ```sh
+    cd /var/www/html
+    rsync -a root@loja.meece.com.br:/var/www/html.tar.gz .
+    tar -xzf html.tar.gz
+    cp zupah/app/etc html/app/etc
+    ln -s /var/www/html/zupah/vendor/zupah zupah
+    mv html src
+    cd src
+    chmod 777 -R *
+    php bin/magento setup:store-config:set --base-url="http://dev.meece.com.br/"
+    php bin/magento setup:store-config:set --base-url-secure="https://dev.meece.com.br/"
+    bin/magento config:set catalog/search/engine 'elasticsearch7'
+    bin/magento config:set catalog/search/elasticsearch7_server_hostname 'meece-elasticsearch'
+    php bin/magento cache:flush
+    php bin/magento indexer:reindex
+    php bin/magento setup:upgrade
+    php bin/magento setup:di:compile
+    php bin/magento setup:static-content:deploy -f
+    chmod 777 -R pub var generated
+  ```
---
+++ zupah/app/etc/env.php
@@ -0,0 +1,134 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'aa7f608581a8b1913a4316d3bc7fdf6b'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'meece-mysql',
+                'dbname' => 'magento',
+                'username' => 'root',
+                'password' => 'root',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'production',
+    'session' => [
+        'save' => 'redis',
+        'redis' => [
+            'host' => 'meece-redis',
+            'port' => '6379',
+            'password' => '',
+            'timeout' => '5',
+            'persistent_identifier' => '',
+            'database' => '10',
+            'compression_threshold' => '2048',
+            'compression_library' => 'gzip',
+            'log_level' => '1',
+            'max_concurrency' => '30',
+            'break_after_frontend' => '10',
+            'break_after_adminhtml' => '30',
+            'first_lifetime' => '600',
+            'bot_first_lifetime' => '60',
+            'bot_lifetime' => '7200',
+            'disable_locking' => '1',
+            'min_lifetime' => '60',
+            'max_lifetime' => '2592000',
+            'sentinel_master' => '',
+            'sentinel_servers' => '',
+            'sentinel_connect_retries' => '5',
+            'sentinel_verify_master' => '0'
+        ]
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '69d_',
+                'backend' => 'Magento\\Framework\\Cache\\Backend\\Redis',
+                'backend_options' => [
+                    'server' => 'meece-redis',
+                    'database' => '0',
+                    'port' => '6379',
+                    'password' => '',
+                    'compress_data' => '1',
+                    'compression_lib' => ''
+                ]
+            ],
+            'page_cache' => [
+                'id_prefix' => '69d_',
+                'backend' => 'Magento\\Framework\\Cache\\Backend\\Redis',
+                'backend_options' => [
+                    'server' => 'meece-redis',
+                    'database' => '1',
+                    'port' => '6379',
+                    'password' => '',
+                    'compress_data' => '0',
+                    'compression_lib' => ''
+                ]
+            ]
+        ],
+        'allow_parallel_generation' => false,
+        'graphql' => [
+            'id_salt' => 'SUCyFiDxFe87Ey6Q769yLSFnRXonjAfM'
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'dev.meece.com.br'
+    ],
+    'install' => [
+        'date' => 'Tue, 18 Oct 2022 17:43:55 +0000'
+    ],
+    'cron_consumers_runner' => [
+        'cron_run' => true,
+        'max_messages' => 10000,
+        'consumers' => [
+            'codegeneratorProcessor'
+        ]
+    ]
+];

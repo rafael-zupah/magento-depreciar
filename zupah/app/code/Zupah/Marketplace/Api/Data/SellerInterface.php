2288b0216eb52ca16bfeaedf2644437e31c66a50
Felipe Batista <felipe.batistaa52@gmail.com>
2023-05-18 10:30:01 -0300

    adição de novos campos

--- zupah/app/code/Zupah/Marketplace/Api/Data/SellerInterface.php
+++ zupah/app/code/Zupah/Marketplace/Api/Data/SellerInterface.php
@@ -25,6 +25,12 @@ interface SellerInterface
     public const COMPANY_DESCRIPTION = 'company_description';
     public const RETURN_POLICY = 'return_policy';
     public const SHIPPING_POLICY = 'shipping_policy';
+    public const IS_SELLER = 'is_seller';
+    public const OTHERS_INFO = 'others_info';
+    public const BANNER_PIC = 'banner_pic';
+    public const LOGO_PIC = 'logo_pic';
+    public const CNPJ = 'cnpj';
+
     
     /**
      * Get ID
@@ -85,4 +91,169 @@ interface SellerInterface
      * @return \Zupah\Marketplace\Api\Data\SellerInterface
      */
     public function setCreatedAt($value);
+
+        /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getContactNumber();
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setContactnumber($value);
+
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getCompanyLocality();
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setCompanyLocality($value);
+
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getCompanyDescription();
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setCompanyDescription($value);
+
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getPrivacyPolicy();
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setPrivacyPolicy($value);
+
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getReturnPolicy();
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setReturnPolicy($value);
+    
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getShippingPolicy();
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setShippingPolicy($value);
+
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getIsSeller();
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setIsSeller($value);
+
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getOthersInfo();
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setOthersInfo($value);
+
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getBannerPic();
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setBannerPic($value);
+
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getLogoPic();
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setLogoPic($value);
+
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getCnpj();
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setCnpj($value);
 }
--- zupah/app/code/Zupah/Marketplace/Model/Seller.php
+++ zupah/app/code/Zupah/Marketplace/Model/Seller.php
@@ -71,5 +71,235 @@ class Seller extends \Webkul\Marketplace\Model\Seller implements SellerInterface
     {
         return $this->setData(SellerInterface::CREATED_AT, $value);
     }
+     /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getContactNumber()
+    {
+        return parent::getData(SellerInterface::CONTACT_NUMBER);
+    }
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setContactnumber($value)
+    {
+        return $this->setData(SellerInterface::CONTACT_NUMBER, $value);
+    }
+
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getCompanyLocality()
+    {
+        return parent::getData(SellerInterface::COMPANY_LOCALITY);
+    }
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setCompanyLocality($value)
+    {
+        return $this->setData(SellerInterface::COMPANY_LOCALITY, $value);
+    }
+
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getCompanyDescription()
+    {
+        return parent::getData(SellerInterface::COMPANY_DESCRIPTION);
+    }
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setCompanyDescription($value)
+    {
+        return $this->setData(SellerInterface::COMPANY_DESCRIPTION, $value);
+    }
+
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getPrivacyPolicy()
+    {
+        return parent::getData(SellerInterface::PRIVACY_POLICY);
+    }
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setPrivacyPolicy($value)
+    {
+        return $this->setData(SellerInterface::PRIVACY_POLICY, $value);
+    }
+
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getReturnPolicy()
+    {
+        return parent::getData(SellerInterface::RETURN_POLICY);
+    }
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setReturnPolicy($value)
+    {
+        return $this->setData(SellerInterface::RETURN_POLICY, $value);
+    }
+    
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getShippingPolicy()
+    {
+        return parent::getData(SellerInterface::SHIPPING_POLICY);
+    }
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setShippingPolicy($value)
+    {
+        return $this->setData(SellerInterface::SHIPPING_POLICY, $value);
+    }
+
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getIsSeller()
+    {
+        return parent::getData(SellerInterface::IS_SELLER);
+    }
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setIsSeller($value)
+    {
+        return $this->setData(SellerInterface::IS_SELLER, $value);
+    }
+
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getOthersInfo()
+    {
+        return parent::getData(SellerInterface::OTHERS_INFO);
+    }
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setOthersInfo($value)
+    {
+        return $this->setData(SellerInterface::OTHERS_INFO, $value);
+    }
+
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getBannerPic()
+    {
+        return parent::getData(SellerInterface::BANNER_PIC);
+    }
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setBannerPic($value)
+    {
+        return $this->setData(SellerInterface::BANNER_PIC, $value);
+    }
+
+    /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getLogoPic()
+    {
+        return parent::getData(SellerInterface::LOGO_PIC);
+    }
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setLogoPic($value)
+    {
+        return $this->setData(SellerInterface::LOGO_PIC, $value);
+    }
+
+     /**
+     * Get
+     *
+     * @return string|null
+     */
+    public function getCnpj()
+    {
+        return parent::getData(SellerInterface::CNPJ);
+    }
+
+    /**
+     * Set
+     *
+     * @param string $value
+     * @return \Zupah\Marketplace\Api\Data\SellerInterface
+     */
+    public function setCnpj($value)
+    {
+        return $this->setData(SellerInterface::CNPJ, $value);
+    }
 
 }
